d20ee1d244a93d72e7c544e192de0f56440cec80
VahidN <vahid_nasiri@yahoo.com>
2018-04-06 12:05:24 +0430

    clean up

--- src/ASPNETCore2JwtAuthentication.ConsoleClient/Program.cs
+++ src/ASPNETCore2JwtAuthentication.ConsoleClient/Program.cs
@@ -4,7 +4,6 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
