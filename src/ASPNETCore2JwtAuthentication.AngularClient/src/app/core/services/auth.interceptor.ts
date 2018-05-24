80196cf9e70367002ac770928d261e3edf8611a4
VahidN <vahid_nasiri@yahoo.com>
2018-05-24 11:45:27 +0430

    Retrying Failed Http Requests

--- src/ASPNETCore2JwtAuthentication.AngularClient/src/app/core/services/auth.interceptor.ts
+++ src/ASPNETCore2JwtAuthentication.AngularClient/src/app/core/services/auth.interceptor.ts
@@ -1,14 +1,17 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
-import { Observable, throwError } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { Observable, of, throwError } from "rxjs";
+import { catchError, delay, mergeMap, retryWhen, take } from "rxjs/operators";
 
 import { AuthTokenType } from "./../models/auth-token-type";
 import { TokenStoreService } from "./token-store.service";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+
+  private delayBetweenRetriesMs = 1000;
+  private numberOfRetries = 3;
 
   constructor(
     private tokenStoreService: TokenStoreService,
@@ -21,6 +24,27 @@ export class AuthInterceptor implements HttpInterceptor {
         headers: request.headers.set("Authorization", `Bearer ${accessToken}`)
       });
       return next.handle(request).pipe(
+        retryWhen(errors => {
+
+          return errors.pipe(
+            mergeMap((error: HttpErrorResponse, retryAttempt: number) => {
+              if (retryAttempt === this.numberOfRetries - 1) {
+                console.log(`HTTP call '${request.method} ${request.url}' failed after ${this.numberOfRetries} retries.`);
+                return throwError(error); // no retry
+              }
+
+              switch (error.status) {
+                case 400:
+                case 404:
+                  return throwError(error); // no retry
+              }
+
+              return of(error); // retry
+            }),
+            delay(this.delayBetweenRetriesMs),
+            take(this.numberOfRetries)
+          );
+        }),
         catchError((error: any, caught: Observable<HttpEvent<any>>) => {
           console.error({ error, caught });
           if (error.status === 401 || error.status === 403) {
@@ -34,4 +58,6 @@ export class AuthInterceptor implements HttpInterceptor {
       return next.handle(request);
     }
   }
+
+
 }
